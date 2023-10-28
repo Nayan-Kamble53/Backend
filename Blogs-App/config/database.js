48c4fb14aab773a4d6a3a5c8d5db77c2cea9a49c
Nayan-Kamble53 <96567359+Nayan-Kamble53@users.noreply.github.com>
2023-10-28 13:50:39 +0530

    added configs

---
+++ Blogs-App/config/database.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const dbConnect = () => {
+    mongoose.connect(process.env.DATABASE_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(console.log('DB Connected Successfully'))
+    .catch((err) => {
+        console.log("Error occurred");
+        console.error(err);
+        process.exit(1); 
+    }) 
+}
+
+module.exports = dbConnect;
