cfbc3f20380cb0844969e6d3a5ef13ffa5307bc9
Nayan Kamble <96567359+Nayan-Kamble53@users.noreply.github.com>
2024-01-15 14:23:37 +0530

    added configs

---
+++ File upload/config/database.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+exports.dbConnect = () => {
+    mongoose.connect(process.env.MONGODB_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(console.log('DB connected successfully!'))
+    .catch((error) => {
+        console.log('Error in DB connection');
+        console.error(error);
+        process.exit(1);
+    });
+}
